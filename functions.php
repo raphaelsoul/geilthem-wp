175f795e274b168589802f408289f715513280b5
raphaelsoul <raphaelsoul@163.com>
2015-12-02 12:42:25 +0900

    rebuild with material design

--- functions.php
+++ functions.php
@@ -1,15 +1,28 @@
 <?php
-//source intro
-function geilcode_add_styles() {
-	/*register source*/
-	wp_register_script('mdl-style',get_template_directory_uri() . '/assets/plugins/mdl/material.indigo-blue.min.css');
-	wp_register_script('mdl-script',get_template_directory_uri() . '/assets/plugins/mdl/material.min.js');
+function intro_mdl() {//引入mdl
+	wp_register_style('mdl-style',get_template_directory_uri() . '/assets/plugins/mdl/material.min.css',false,false,false);
+	wp_register_style('mdl-custom',get_template_directory_uri() . '/assets/plugins/mdl/custom.css',false,false,false);
+	wp_register_script('mdl-script',get_template_directory_uri() . '/assets/plugins/mdl/material.min.js',false,false,false);
 	
-	/*intro source*/
+	//intro source
 	if(!is_admin()) {
-		wp_enqueue_script('mdl-style');
+		wp_enqueue_style('mdl-style');
+		wp_enqueue_style('mdl-custom');
 		wp_enqueue_script('mdl-script');
 	}
 }
+add_action( 'init', 'intro_mdl' );
 
-add_action( 'init', 'geilcode_add_styles' );
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
+
+//移除google字体
+function remove_open_sans_from_wp_core() {
+	wp_deregister_style( 'open-sans' );   
+	wp_register_style( 'open-sans', false );   
+	wp_enqueue_style('open-sans','');
+}
+add_action( 'init', 'remove_open_sans_from_wp_core' );
+
--- header.php
+++ header.php
@@ -1,3 +1,18 @@
-<header>
-I am header
-</header>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+	<?php wp_head(); // wp_head ?>
+
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <style>
+    #view-source {
+      position: fixed;
+      display: block;
+      right: 0;
+      bottom: 0;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      z-index: 900;
+    }
+    </style>
+  </head>
